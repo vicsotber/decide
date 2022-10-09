a9d5ca32d4bb2cf78dc5626c0dcb1d738b50baca
vicsotber <victor.soto.berenguer123@gmail.com>
2022-10-09 21:47:53 +0200

    Added local_settings.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -109,9 +109,9 @@ WSGI_APPLICATION = 'decide.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'decide',
+        'NAME': 'decidedb',
         'USER': 'decide',
-        'PASSWORD': 'decide',
+        'PASSWORD': 'complexpassword',
         'HOST': 'localhost',
         'PORT': '5432',
     }
